d880781948e9439734d34cb0b4ac0f9a31e9b3a2
MerelVolgers <70475252+MerelVolgers@users.noreply.github.com>
2020-10-07 16:15:40 +0200

    Add files via upload

---
+++ Opdracht020/dist/index.dev.js
@@ -0,0 +1,13 @@
+"use strict";
+
+console.log("Hello Winc Academy");
+var name = 'Merel';
+console.log(name);
+console.log(4 + 4);
+console.log('4' + '4');
+console.log(2 + 6);
+console.log(2 - 6);
+console.log(2 * 6);
+console.log(2 / 6);
+console.log(2 % 6);
+var age = 30;
---
+++ Opdracht020/index.js
@@ -0,0 +1,14 @@
+console.log("Hello Winc Academy");
+let name = 'Merel';
+console.log(name);
+
+console.log(4 + 4);
+console.log('4' + '4');
+
+console.log(2 + 6);
+console.log(2 - 6);
+console.log(2 * 6);
+console.log(2 / 6);
+console.log(2 % 6);
+
+let age = 30;
