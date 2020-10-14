8b55e536ef8909b62487b4aec0ed84055faef101
MerelVolgers <70475252+MerelVolgers@users.noreply.github.com>
2020-10-14 13:52:51 +0200

    Add files via upload

---
+++ Opdracht022/script.js
@@ -0,0 +1,23 @@
+const age = 18;
+
+if (age >= 18) {
+    console.log("Mag naar binnen");
+} else {
+    console.log("mag niet naar binnen");
+}
+
+const isFemale = true;
+
+if (isFemale) {
+    console.log("True");
+} else {
+    console.log("false");
+}
+
+const driverStatus = "bob";
+
+if (diverStatus === "bob") {
+    console.log("U mag rijden");
+} else {
+    console.log("U mag niet rijden");
+}
